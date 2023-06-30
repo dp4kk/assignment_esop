af28b27efab86c2109a9d63f3a68d36c45a5ea50
Deepak kushwaha <kushwahadeepak797@gmail.com>
2023-06-30 11:41:12 +0545

    completed welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,22 +4,22 @@ const Welcome = () => {
   return (
     <div className="w-full relative py-24 overflow-hidden">
       <div className="max-w-[1240px] px-4 mx-auto">
-        <div className="flex items-center flex-wrap mx-[-15px]">
-          <div className="relative w-3/5">
-            <img src={image} alt="school" />
+        <div className="flex items-center  flex-wrap sm:flex-col overflow-visible md:flex-row-reverse lg:flex-row ">
+          <div className="relative lg:w-3/5 md:w-2/5 sm:px-10 box-border bg-[#242d69]">
+            <img src={image} alt="school" className="mt-[-40px] mb-[40px] ml-[40px]" />
           </div>
-          <div className="relative w-2/5">
+          <div className="relative lg:w-2/5 md:w-3/5 sm:4/5 bg-white">
             <div className="py-0 px-8">
               <div className="relative mb-8">
-                <h2 className="mb-0 text-[#242d69] text-4xl">
+                <h2 className="mb-0 text-[#242d69] text-4xl  ">
                   Welcoming, Imperial Heritage School
                 </h2>
               </div>
               <p className="text-lg mb-4">
                 At{" "}
-                <e className="text-[#c19753] italic">
+                <span className="text-[#c19753] italic">
                   Imperial Heritage School
-                </e>
+                </span>
                 , we always strive to present our students with high quality
                 education that helps to forge an understanding that strengthens
                 bonds across languages and cultures, across borders and social
@@ -31,8 +31,11 @@ const Welcome = () => {
                 children.
               </p>
               <div className="mt-8 w-full">
-                <a className="relative z-1 flex items-center gap-[10px] px-[15px] py-[10px] min-w-[130px] max-w-max rounded-full text-primary-color bg-transparent border border-[#242d69] text-center transition duration-300 ease-in-out">
-                Learn more
+                <a className="relative z-1 flex items-center gap-[10px] px-[15px] py-[10px] min-w-[125px] max-w-max h-[42px]    rounded-full text-primary-color bg-transparent border border-[#242d69] text-center transition duration-300 ease-in-out group hover:text-white hover:bg-[#242d69] ">
+                  Learn more
+                  <div className=" bg-[#c19753] w-8 h-8 rounded-full group-hover:translate-x-4 group-hover:w-10 group-hover:h-10 flex justify-center items-center">
+                    <span className=" text-white ">→</span>
+                  </div>
                 </a>
               </div>
             </div>
