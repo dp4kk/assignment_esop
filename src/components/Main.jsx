324cd6582c4c63c9f3b0df6e61a49a61f2a0cd97
Deepak kushwaha <kushwahadeepak797@gmail.com>
2023-07-01 19:03:41 +0545

    finished main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import banner03 from '../assets/images/banner3.jpg'
 import banner04 from '../assets/images/banner4.jpg'
 import banner05 from '../assets/images/banner5.jpg'
 
+
 const carouselItems = [
   {
     id: 1,
@@ -46,7 +47,7 @@ const Main = () => {
       () => {
         setShowText((prevValue) => !prevValue);
       },
-      showText ? 5000 : 100
+      showText ? 5000 : 1
     );
 
     return () => {
@@ -65,39 +66,68 @@ const Main = () => {
               alt="banner"
               className="z-0 animate-zoom-in scale-100 transition-transform duration-200 ease-in-out "
             />
-          </div>
-
-          {showText && (
-            <div className="w-[70%] absolute top-1/2 left-1/2 -translate-x-1/2 text-center transition-all duration-500 ease-linear delay-500 z-10 ">
-              <div className="animate-slide-up">
-                <p className="text-white text-3xl">
-                  Best School in New Gurgaon
-                </p>
-                <p className="text-white text-6xl uppercase">
-                  Imperial Heritage School
-                </p>
+            {showText && (
+              <div className="w-[70%] absolute top-1/2 left-1/2 -translate-x-1/2 text-center transition-all duration-500 ease-linear delay-500 z-10 ">
+                <div className="animate-slide-up">
+                  <p className="text-white text-3xl">
+                    Best School in New Gurgaon
+                  </p>
+                  <p className="text-white text-6xl uppercase">
+                    Imperial Heritage School
+                  </p>
+                </div>
               </div>
-            </div>
-          )}
-          <div className="fixed flex flex-row  bottom-[20px] right-[30px]">
+            )}
+          </div>
+          <div className="fixed flex   bottom-[20px] right-[30px]">
             <div className="flex items-center flex-row justify-between z-10">
-              <ul className="static items-center mr-[10px] ml-0">
-                <li className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs">
+              <ul className="static flex items-center mr-[10px] ml-0">
+                <li
+                  onClick={() => setActiveIndex(0)}
+                  className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs hover:cursor-pointer hover:underline"
+                >
                   01
                 </li>
-                <li className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs">
-                  01
+                <li
+                  onClick={() => setActiveIndex(1)}
+                  className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs hover:cursor-pointer hover:underline"
+                >
+                  02
                 </li>
-                <li className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs">
-                  01
+                <li
+                  onClick={() => setActiveIndex(2)}
+                  className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs hover:cursor-pointer hover:underline"
+                >
+                  03
                 </li>
-                <li className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs">
-                  01
+                <li
+                  onClick={() => setActiveIndex(3)}
+                  className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs hover:cursor-pointer hover:underline"
+                >
+                  04
                 </li>
-                <li className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs">
-                  01
+                <li
+                  onClick={() => setActiveIndex(4)}
+                  className="width-auto height-auto bg-transparent opacity-100 text-white mx-[10px] text-indent-inherit border-t-0 border-b-0 font-semibold text-xs hover:cursor-pointer hover:underline"
+                >
+                  05
                 </li>
               </ul>
+              <div className="flex items-center text-center">
+                <span className="w-[100px] mr-1 border-b"></span>
+                <a
+                  className="flex items-center justify-center text-white text-center hover:cursor-pointer"
+                  onClick={() => setActiveIndex((prev)=>prev===0?4:prev-1)}
+                >
+                  <span className="inline-block  w-[40px] [h-40px]">＜</span>
+                </a>
+                <a
+                  className=" flex items-center justify-center text-white text-center fill-white hover:cursor-pointer "
+                  onClick={() => setActiveIndex((prev) =>prev===4?0: prev + 1)}
+                >
+                  <span className="inline-block  w-[40px] [h-40px]">＞</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
